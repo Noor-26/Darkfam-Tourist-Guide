ebed376154076442c2c8ab25ccd609ce61257e27
Noor-26 <nk26982698@gmail.com>
2022-04-16 22:30:46 +0600

    components stracture completed

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import auth from '../../firebase.init';
+import './Login.css';
+const Login = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import auth from '../../firebase.init';
+
+const Register = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Register;
