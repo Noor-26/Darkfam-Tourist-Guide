f0c206b130a6c9fb2e0f5f6917c819f1afadd906
Noor-26 <nk26982698@gmail.com>
2022-04-17 00:17:37 +0600

    banner responsive and navber component added

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand className="head-text" as={Link} to="/">Darkfam-Tourist-guide</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link as={Link} to="/service">Service</Nav.Link>
+      <Nav.Link as={Link} to="/login">Login</Nav.Link>
       
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+    <Nav.Link as={Link} to="/about">About</Nav.Link>
+      <Nav.Link as={Link} to="/login">Login</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
